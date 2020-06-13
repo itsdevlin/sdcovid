32249a7960586b88df56c617da2a9162f48eb959
Sean Devlin <sean.devlin@housecallpro.com>
2020-06-13 16:38:33 -0700

    v0

--- scraper.rb
+++ scraper.rb
@@ -1,47 +1,32 @@
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
+require 'csv'
 
-def scraper
-	url = "https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html"
-	unparsed_page = HTTParty.get(url)
-	parsed_page = Nokogiri::HTML(unparsed_page)
-	full_table = parsed_page.css('div.table.parbase.section')
-	full_table.search('tr').each do |tr|
-	cells = tr.search('th, td')
-	puts cells.text
-	end
+timestamp = Time.now.to_i
+time_formatted = Time.now.strftime("%m/%d/%Y %k:%M")
+url = "https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html"
+unparsed_page = HTTParty.get(url)
+parsed_page = Nokogiri::HTML(unparsed_page)
+full_table = parsed_page.css('div.table.parbase.section')
+rows = full_table.css('tr') # get all rows (<tr>s)
 
-	byebug
-
-
+text_all_rows = rows.map do |row|
+  row_values = row.css('td').map(&:text) # get the text of each individual value (<td>)
+  # I don't totally understand what this is doing, but text_all_rows is an array with the values as desired
 end
 
-scraper
+text_all_rows.shift(1)
 
-# div.table.parbase.section
-
+CSV.open("#{timestamp}.csv", "w") do |csv|
+  csv << ["Data as of","#{time_formatted}"]
+  text_all_rows.each {|row| csv << row } 
+end
 
 =begin
 	
-
-
-(byebug) full_table = parsed_page.css('div.table.parbase.section')
-
-full_table.css('td').first
-// full first row of everything
-
-full_table.css('tr').first.text
-// text strings of the first row
-
-full_table.css('tr')[0]
-// first row 
-
+https://stackoverflow.com/questions/34781600/how-to-parse-a-html-table-with-nokogiri
+https://ruby-doc.org/stdlib-2.7.1/libdoc/csv/rdoc/CSV.html
+https://www.rubyguides.com/2015/12/ruby-time/
 	
-
-
-full_table.search('tr').each do |tr|
-	cells = tr.search('th, td')
-end
-
 =end
